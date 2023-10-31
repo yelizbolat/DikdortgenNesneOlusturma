08d8e370d84a4edfe5463c32e4188327a5ef3663
bolat <bolat@YELIZ>
2023-10-31 11:56:44 +0300

    Proje dosyası ekle.

---
+++ DikdortgenNesneOlusturma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenNesneOlusturma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(3,4);
+            Console.WriteLine("Dikdorgenin Alani = "+dikdortgen.alanHesapla());
+            Console.WriteLine("Dikdorgenin Cevresi = " + dikdortgen.cevreHesapla());
+        }
+    }
+    class Dikdortgen
+    {
+        private uint kisaKenar, uzunKenar;
+        public Dikdortgen(uint kisaKenar, uint uzunKenar)
+        {
+            this.kisaKenar = kisaKenar;
+            this.uzunKenar = uzunKenar;
+        }
+        public uint alanHesapla()
+        {
+            return kisaKenar  * uzunKenar;
+        }
+        public uint cevreHesapla()
+        {
+            return 2 * (kisaKenar + uzunKenar);
+        }
+    }
+}
